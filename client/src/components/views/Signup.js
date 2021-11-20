9f9ef4ef88d324045b3a3e7ee287b3d68cf1d31f
clippefuentes <clippefuentes@gmail.com>
2021-11-19 22:26:48 -0700

    Add use state for signu

--- client/src/components/views/Signup.js
+++ client/src/components/views/Signup.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Signup = () => {
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    const PostData = async () => {
+        const res = await fetch('http://localhost:5000/signup', {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name, password, email
+            })
+        })
+        const data = await res.json()
+    }
     return (
         <div className="my-card">
             <div className="card auth-card input-field">
                 <h2 className="brand-logo"> Clynestagram </h2>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">
+                <input type="text" placeholder="Name" value={name} onChange={(e) => { setName(e.target.value)}}/>
+                <input type="email" placeholder="Email" value={password} onChange={(e) => { setPassword(e.target.value)}}/>
+                <input type="password" placeholder="Password" value={email} onChange={(e) => { setEmail(e.target.value)}}/>
+                <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action" onClick={PostData}>
                     Sign Up
                 </button>
                 <h5>
