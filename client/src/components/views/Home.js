76900e733cb8e38f7da9ad0cb1b55fb7947827ae
clippefuentes <clippefuentes@gmail.com>
2021-11-20 23:02:39 -0700

    implement like/dislike

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { UserContext } from '../../App'
 
 const Home = () => {
+    const { state, dispatch } = useContext(UserContext)
     const [data, setData] = useState([])
 
     useEffect(() => {
@@ -12,11 +14,58 @@ const Home = () => {
             })
             const resJson = await res.json()
             const posts = resJson.posts
+            console.log('posts:', posts)
             setData(posts)
         })();
 
     }, [])
 
+    const likePost = async (item) => {
+        const res = await fetch('/like', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem('jwt')
+            },
+            body: JSON.stringify({
+                postId: item._id
+            })
+        })
+        const resJson = await res.json()
+        const post = resJson.post
+        const newData = data.map(item => {
+            if (item._id == post._id) {
+                return post
+            } else {
+                return item
+            }
+        })
+        setData(newData)
+    }
+
+
+    const unlikePost = async (item) => {
+        const res = await fetch('/unlike', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem('jwt')
+            },
+            body: JSON.stringify({
+                postId: item._id
+            })
+        })
+        const resJson = await res.json()
+        const post = resJson.post
+        const newData = data.map(item => {
+            if (item._id == post._id) {
+                return post
+            } else {
+                return item
+            }
+        })
+        setData(newData)
+    }
     return (
         <div className="home">
             {
@@ -29,6 +78,25 @@ const Home = () => {
                             </div>
                             <div className="card-content">
                                 <i className="material-icons" style={{ color: 'black' }}>favorite</i>
+                                {
+                                    
+                                }
+                                {
+                                    item.likes.includes(state._id) ? (
+                                        <i className="material-icons"
+                                            style={{ color: 'black' }}
+                                            onClick={() => { unlikePost(item) }}
+                                        >thumb_down</i>
+                                    ) : (
+
+                                        <i className="material-icons"
+                                            style={{ color: 'black' }}
+                                            onClick={() => { likePost(item) }}
+                                        >thumb_up</i>
+                                    )
+                                }
+
+                                <h6>{item.likes ? item.likes.length : 0} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.caption}</p>
                                 <input type="text" placeholder="Add a comment" />
--- server/routes/post.js
+++ server/routes/post.js
@@ -43,4 +43,44 @@ router.get('/myPosts', requiredLogin, async (req, res) => {
     return res.json({ posts: userPosts })
 })
 
+router.put('/like', requiredLogin,  async (req, res) => {
+    try {
+        const { postId } = req.body
+        if (!postId) {
+            return res.status(422).json({ error: "Please add all the fields" })
+        }
+        const post = await Post.findByIdAndUpdate(postId, {
+            $push: {
+                likes: req.user._id
+            }
+        }, {
+            new: true
+        }).exec()
+        return res.json({ post: post })
+    } catch (err) {
+        console.log('err', err)
+        return res.json({ error: err })
+    } 
+})
+
+router.put('/unlike', requiredLogin,  async (req, res) => {
+    try {
+        const { postId } = req.body
+        if (!postId) {
+            return res.status(422).json({ error: "Please add all the fields" })
+        }
+        const post = await Post.findByIdAndUpdate(postId, {
+            $pull: {
+                likes: req.user._id
+            }
+        }, {
+            new: true
+        }).exec()
+        return res.json({ post: post })
+    } catch (err) {
+        console.log('err', err)
+        return res.json({ error: err })
+    } 
+})
+
 module.exports = router
